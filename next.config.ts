704ab26514788f7d6651dee7729b2d4f55feadc1
siefm <siefm@users.noreply.github.com>
2026-06-22 19:18:00 +0300

    fix: disable image optimization due to unsplash query params in DB

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        search: "?*",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-      },
-      {
-        protocol: "https",
-        hostname: "assets.m4vx.com",
-      },
-    ],
+    unoptimized: true,
   },
   async headers() {
     return [
